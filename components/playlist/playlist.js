7920c268331d4991094f6fafe65c9643a5feac57
cseswapon <cseswaponsaha@gmail.com>
2025-03-08 22:06:53 +0600

    [ADD] - added dropdown system with playlist colapsable slide

--- components/playlist/playlist.js
+++ components/playlist/playlist.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { MdOutlineArrowDropDown } from "react-icons/md";
 import SmallThumbnail from "../small-thumbnail";
 
 const data = {
@@ -9,34 +11,42 @@ const data = {
 };
 
 export default function VideoPlaylist() {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(true);
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
   return (
     <div className="max-w-lg mb-4 bg-white text-black rounded-lg shadow border overflow-hidden">
-      <div className="p-4 border-b border-gray-700">
-        <h2 className="text-lg font-bold">নামাজ পড়লে হবে কিনা?</h2>
-        <p className="text-sm text-gray-400">
-          নামাজ পড়লে হবে কিনা - 1 / 100
-        </p>
-      </div>
       <div className="relative">
-        {/*  <Image
-          src={selected.thumbnail}
-          alt={selected.title}
-          className="w-full h-40 object-cover"
-        /> */}
-        <button className="absolute inset-0 flex items-center justify-center text-white text-4xl">
-          {/* <FaPlay /> */}
+        <div className="p-4 border-b border-gray-100 border-opacity-90 ">
+          <h2 className="text-lg font-bold">নামাজ পড়লে হবে কিনা?</h2>
+          <p className="text-sm text-gray-400">নামাজ পড়লে হবে কিনা - 1 / 100</p>
+        </div>
+        <button
+          onClick={toggleDropdown}
+          className="absolute text-black text-lg top-4 right-2"
+        >
+          <MdOutlineArrowDropDown className={`inline-block text-4xl transition-all duration-300 ease-in-out ${isDropdownOpen ? '-rotate-180' : 'rotate-0'}`} />
         </button>
       </div>
-      <div className="p-4 h-4/6 overflow-y-auto">
-        {Array(10)
-          .fill()
-          .map((item, i) => (
-            <SmallThumbnail
-              key={i}
-              data={data}
-              className="basis-4/12 mb-3 block"
-            />
-          ))}
+
+      <div
+        className={` overflow-y-auto transition-all duration-500 ease-in-out ${
+          isDropdownOpen ? "max-h-[70vh]" : "max-h-0"
+        }`}
+      >
+        <div className="p-4">
+          {Array(10)
+            .fill()
+            .map((item, i) => (
+              <SmallThumbnail
+                key={i}
+                data={data}
+                className="basis-4/12 mb-3 block"
+              />
+            ))}
+        </div>
       </div>
     </div>
   );
